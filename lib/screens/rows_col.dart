ef0d41c442292deded595ad6173659514d1858ff
Kimanzi <kimanzi@localhost.localdomain>
2023-11-01 15:34:13 +0300

    rows col

--- lib/screens/rows_col.dart
+++ lib/screens/rows_col.dart
@@ -7,13 +7,55 @@ class RC extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Rows and Columns'), centerTitle: true,),
-      body: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: const Column(
         children: [
-          Icon(Icons.phone, size: 35,),
-          Icon(Icons.alarm_add, size: 35,),
-          Icon(Icons.card_travel, size: 35,),
-          Icon(Icons.date_range, size: 35,),
+          Row(
+            children: [
+              Expanded(child: Image(image: AssetImage('images/kim.jpg'), width: 150,)),
+              Expanded(flex:2, child: Image(image: AssetImage('images/kim.jpg'), width: 150,)),
+              Expanded(child: Image(image: AssetImage('images/kim.jpg'), width: 150,)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star_border),
+              Icon(Icons.star_border),
+            ],
+          ), 
+          Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Column(
+              children: [
+              Icon(Icons.phone, size: 35,),
+              Text('Phone')
+              ],
+            ),
+            Column(
+              children: [
+              Icon(Icons.alarm_add, size: 35,),
+              Text('Alarm')
+              ],
+            ),
+            Column(
+              children: [
+              Icon(Icons.card_giftcard, size: 35,),
+              Text('Gift')
+              ],
+            ),
+            Column(
+              children: [
+              Icon(Icons.carpenter_outlined, size: 35,),
+              Text('Carpenter')
+              ],
+            ),
+          ],
+      ),
         ],
       ),
     );
