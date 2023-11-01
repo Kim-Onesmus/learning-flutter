b37d7d6b17c2ae13044419aa3132d573af7d8a63
Kim-Onesmus <kimones1122@gmail.com>
2023-11-01 16:09:43 +0300

    listview

--- lib/screens/listview.dart
+++ lib/screens/listview.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
-
 class LV extends StatelessWidget {
-  const LV({super.key});
+  LV({super.key});
+
+  List<String> products = ['bed', 'sofa', 'chair'];
+  List<String> productDetails = ['6 by 6', 'Leather', 'plastic'];
+  List<String> price = ['3000', '7000', '1000'];
+  
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: (
-        
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List View'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            return Text(products[index]);
+          },
+        ),
       ),
     );
   }
